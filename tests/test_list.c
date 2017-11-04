9731216d596876e336d89db2c2bfd18a16c49c2f
DuccioLenkowicz <ducciolenkowicz@gmail.com>
2017-11-04 21:36:29 +0100

    added test_aiv_list_contains_at

--- tests/test_list.c
+++ tests/test_list.c
@@ -806,6 +806,48 @@ int test_aiv_list_contains_at_out_of_range()
     return res;
 }
 
+int test_aiv_list_contains_at()
+{
+    aiv_list_t *list = aiv_list_new(NULL);
+    if(!list)
+        return -1;
+    
+    int res = 0;
+
+    const char *c = " Hello";
+
+    int a = 99;
+
+    res = aiv_list_append(list, &c);
+    if(res)
+    {
+        aiv_list_destroy(list);
+        return res;
+    }
+
+    res = aiv_list_append(list, &a);
+    if(res)
+    {
+        aiv_list_destroy(list);
+        return res;
+    }
+
+
+    res = aiv_list_contains_at(list, &a, 1);
+    if(res)
+    {
+        aiv_list_destroy(list);
+        return res;
+    }
+
+    res = aiv_list_contains_at(list, &c, 0);
+
+    aiv_list_destroy(list);
+
+    return res;
+}
+
+
 
 //TODO : contains at, len, slow len, sublist
 void test_list_run()
@@ -836,4 +878,5 @@ void test_list_run()
     test_equal(test_aiv_list_contains_not_contained, AIV_NOT_FOUND);
     test_equal(test_aiv_list_contains_at_empty, AIV_NOT_FOUND);
     test_equal(test_aiv_list_contains_at_out_of_range, AIV_NOT_FOUND);
+    test(test_aiv_list_contains_at);
 }
